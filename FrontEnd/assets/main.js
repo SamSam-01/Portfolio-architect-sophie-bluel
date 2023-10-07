ab9d0e110b534d2c16126b3bf5bb849456f77e0b
Samuel Blard <contact@samuelblard.com>
2023-10-07 14:22:00 +0200

    optimizing js code

--- FrontEnd/assets/main.js
+++ FrontEnd/assets/main.js
@@ -1,25 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const portfolio = document.querySelector("#portfolio .gallery");
-  const filterButtons = document.querySelector("#filter-buttons");
-  console.log(localStorage.getItem("token"));
+const portfolio = document.querySelector("#portfolio .gallery");
+const filterButtons = document.querySelector("#filter-buttons");
+const token = localStorage.getItem("token");
+const categories = new Set();
+const loginButton = document.getElementById("login-button");
+const portfolioContent = document.getElementById("portfolio-content");
+const loginForm = document.getElementById("login-form");
 
-  const token = localStorage.getItem("token");
-  if (token) {
-      document.getElementById("login-button").textContent = "logout";
+async function get_data(type) {
+  try {
+    const response = await fetch("http://localhost:5678/api/" + type);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      console.error(`Erreur HTTP: ${response.status}`);
+      return null;
+    }
+  } catch (error) {
+    console.error("Une erreur s'est produite:", error);
+    return null;
   }
+}
 
-  // Générer les boutons de filtre
-  const categories = new Set(); // Utilisation d'un Set pour des valeurs uniques
+async function updateWorks() {
+  try {
+    // Supprimer les anciens éléments
+    while (portfolio.firstChild) {
+      portfolio.removeChild(portfolio.firstChild);
+    }
+    while (filterButtons.firstChild) {
+      filterButtons.removeChild(filterButtons.firstChild);
+    }
 
-  fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((works) => {
+    const works = await get_data("works");
+    const categoriesData = await get_data("categories");
+
+    if (works && categoriesData) {
       works.forEach((work) => {
-        categories.add(work.category.name); // Ajouter la catégorie à notre Set
-
         const figure = document.createElement("figure");
-        figure.classList.add("project"); // Ajout d'une classe pour faciliter le filtrage
-        figure.dataset.category = work.category.name; // Ajout d'un attribut de catégorie
+        figure.classList.add("project");
+        figure.dataset.category = work.category.name;
 
         const img = document.createElement("img");
         img.src = work.imageUrl;
@@ -36,58 +56,121 @@ document.addEventListener("DOMContentLoaded", () => {
       // Bouton "Tous"
       const allButton = document.createElement("button");
       allButton.innerText = "Tous";
-      allButton.classList.add("active"); // Classe active par défaut
+      allButton.classList.add("active");
       allButton.addEventListener("click", () => filterProjects(null));
       filterButtons.appendChild(allButton);
-      // Créer des boutons de filtre basés sur les catégories
-      categories.forEach(category => {
+
+      // Ajout des catégories depuis la requête
+      categoriesData.forEach(category => {
         const button = document.createElement("button");
-        button.innerText = category;
-        button.addEventListener("click", () => filterProjects(category));
+        button.innerText = category.name;
+        button.addEventListener("click", () => filterProjects(category.name));
         filterButtons.appendChild(button);
       });
-    })
-    .catch((error) => {
-      console.error("Une erreur s'est produite:", error);
-    });
-
-    function filterProjects(category) {
-      document.querySelectorAll(".project").forEach(project => {
-        project.style.display = (project.dataset.category === category || !category) ? "block" : "none";
-      });
-    
-      // Mettre à jour l'état actif des boutons
-      document.querySelectorAll("#filter-buttons button").forEach(button => {
-        button.classList.remove("active");
-      });
-    
-      if (category) {
-        const activeButton = Array.from(document.querySelectorAll("#filter-buttons button")).find(button => button.innerText === category);
-        if (activeButton) {
-          activeButton.classList.add("active");
-        }
-      } else {
-        const allButton = document.querySelector("#filter-buttons button");
-        if (allButton) {
-          allButton.classList.add("active");
-        }
-      }
     }
-  const loginButton = document.getElementById("login-button");
-  const sectionsToHide = [document.getElementById("introduction"), document.getElementById("portfolio"), document.getElementById("contact")];
+  } catch (error) {
+    console.error("Une erreur s'est produite:", error);
+  }
+}
+
+function filterProjects(category) {
+  document.querySelectorAll(".project").forEach(project => {
+    project.style.display = (project.dataset.category === category || !category) ? "block" : "none";
+  });
+
+  // Mettre à jour l'état actif des boutons
+  document.querySelectorAll("#filter-buttons button").forEach(button => {
+    button.classList.remove("active");
+  });
+
+  if (category) {
+    const activeButton = Array.from(document.querySelectorAll("#filter-buttons button")).find(button => button.innerText === category);
+    if (activeButton) {
+      activeButton.classList.add("active");
+    }
+  } else {
+    const allButton = document.querySelector("#filter-buttons button");
+    if (allButton) {
+      allButton.classList.add("active");
+    }
+  }
+}
+
+function showModalWithImages() {
+  const modal = document.getElementById("modal");
+  const imageGallery = document.querySelector(".image-gallery");
+
+  // Vider la galerie
+  while (imageGallery.firstChild) {
+    imageGallery.removeChild(imageGallery.firstChild);
+  }
+
+  // Ajouter les images
+  const projects = document.querySelectorAll(".project");
+  projects.forEach((project) => {
+    const img = document.createElement("img");
+    img.src = project.querySelector("img").src;
+    imageGallery.appendChild(img);
+  });
+
+  // Afficher le modal
+  modal.style.display = "flex";
+}
+
+function updateStatus(status) {
+  if (status === "connect") {
+    document.getElementById("login-button").textContent = "logout";
+    document.getElementById("editModeBanner").classList.remove("hidden");
+    document.getElementById("login").style.display = "none";
+    portfolioContent.style.removeProperty("display");
+  } else if (status === "disconnect") {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    document.getElementById("editModeBanner").classList.add("hidden");
+    loginButton.textContent = "login";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+  // Écouteur pour fermer le modal
+  document.addEventListener("click", function(event) {
+    if (event.target.id === "modal") {
+      document.getElementById("modal").style.display = "none";
+    }
+  });
+
+  document.querySelector(".close").addEventListener("click", function() {
+    document.getElementById("modal").style.display = "none";
+  });  
+  
+  // Écouteur pour le bouton "Ajouter une photo"
+  document.getElementById("add-photo").addEventListener("click", function() {
+    console.log("add");
+  });
+  
+  // Initialisation du modal
+  document.getElementById("edit-mode").addEventListener("click", showModalWithImages);
+  
+  if (token) {
+    updateStatus('connect');
+  }
+
+  // Initialisation des projets
+  updateWorks();
 
   loginButton.addEventListener("click", () => {
       const loginSection = document.getElementById("login");
       if (loginSection.style.display === "none") {
           loginSection.style.display = "block";
-          sectionsToHide.forEach(section => section.style.display = "none");
+          portfolioContent.style.display = "none";
       } else {
           loginSection.style.display = "none";
-          sectionsToHide.forEach(section => section.style.removeProperty("display"));
+          portfolioContent.style.removeProperty("display");
       }
   });
-  const loginForm = document.getElementById("login-form");
 
+  // Gérer la connexion
   loginForm.addEventListener("submit", async (e) => {
       e.preventDefault();
 
@@ -111,12 +194,7 @@ document.addEventListener("DOMContentLoaded", () => {
           if (data.token) {
               localStorage.setItem("token", data.token);
               localStorage.setItem("userId", data.userId);
-
-              console.log(localStorage.getItem("token"));
-
-              document.getElementById("login").style.display = "none";
-              sectionsToHide.forEach(section => section.style.removeProperty("display"));
-              loginButton.textContent = "logout";
+              updateStatus('connect');
           } else {
               alert("Identifiants incorrects.");
           }
@@ -125,12 +203,11 @@ document.addEventListener("DOMContentLoaded", () => {
           alert("Une erreur est survenue lors de la connexion. Veuillez réessayer.");
       }
   });
+
   // Gérer la déconnexion
   loginButton.addEventListener("click", () => {
     if (loginButton.textContent === "logout") {
-        localStorage.removeItem("token");
-        localStorage.removeItem("userId");
-        loginButton.textContent = "login";
+        updateStatus("disconnect");
     }
   });
 });
